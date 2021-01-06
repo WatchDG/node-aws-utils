ea143831a4a09e4b3306b01bce0b9b81f37e1473
WatchDG <baranov@w3m.co>
2021-01-06 23:05:28 +0300

    add get method for dynamo

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,18 @@
 import {ResultOK, ResultFAIL, ResultOk, tryCatchWrapperAsync} from "node-result";
 import {DynamoDB} from "aws-sdk";
+import {DocumentClient} from "aws-sdk/lib/dynamodb/document_client";
 
 export class DynamoDocumentClient {
+    @tryCatchWrapperAsync
+    static async get(client: DynamoDB.DocumentClient, tableName: DynamoDB.DocumentClient.TableName, where: DynamoDB.DocumentClient.Key): Promise<ResultOK<DynamoDB.DocumentClient.AttributeMap | null> | ResultFAIL<Error>> {
+        const params: DynamoDB.DocumentClient.GetItemInput = {
+            TableName: tableName,
+            Key: where
+        };
+        const {Item} = await client.get(params).promise();
+        return ResultOk(Item || null);
+    }
+
     @tryCatchWrapperAsync
     static async update(client: DynamoDB.DocumentClient, tableName: DynamoDB.DocumentClient.TableName, where: DynamoDB.DocumentClient.Key, data: { [key: string]: any }): Promise<ResultOK<DynamoDB.DocumentClient.AttributeMap> | ResultFAIL<Error>> {
         const params: DynamoDB.DocumentClient.UpdateItemInput = {
