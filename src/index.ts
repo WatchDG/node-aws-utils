426a06f51d4ab31bb8fb03a775920601e1fdbb24
WatchDG <baranov@w3m.co>
2021-01-10 09:48:31 +0300

    add fmt, lint and husky

--- src/index.ts
+++ src/index.ts
@@ -1,48 +1,55 @@
-import {ResultOK, ResultFAIL, ResultOk, tryCatchWrapperAsync} from "node-result";
-import {DynamoDB} from "aws-sdk";
-import {DocumentClient} from "aws-sdk/lib/dynamodb/document_client";
+import { ResultOK, ResultFAIL, ResultOk, tryCatchWrapperAsync } from 'node-result';
+import { DynamoDB } from 'aws-sdk';
 
 export class DynamoDocumentClient {
-    @tryCatchWrapperAsync
-    static async get(client: DynamoDB.DocumentClient, tableName: DynamoDB.DocumentClient.TableName, where: DynamoDB.DocumentClient.Key): Promise<ResultOK<DynamoDB.DocumentClient.AttributeMap | null> | ResultFAIL<Error>> {
-        const params: DynamoDB.DocumentClient.GetItemInput = {
-            TableName: tableName,
-            Key: where
-        };
-        const {Item} = await client.get(params).promise();
-        return ResultOk(Item || null);
-    }
+  @tryCatchWrapperAsync
+  static async get(
+    client: DynamoDB.DocumentClient,
+    tableName: DynamoDB.DocumentClient.TableName,
+    where: DynamoDB.DocumentClient.Key
+  ): Promise<ResultOK<DynamoDB.DocumentClient.AttributeMap | null> | ResultFAIL<Error>> {
+    const params: DynamoDB.DocumentClient.GetItemInput = {
+      TableName: tableName,
+      Key: where
+    };
+    const { Item } = await client.get(params).promise();
+    return ResultOk(Item || null);
+  }
 
-    @tryCatchWrapperAsync
-    static async update(client: DynamoDB.DocumentClient, tableName: DynamoDB.DocumentClient.TableName, where: DynamoDB.DocumentClient.Key, data: { [key: string]: any }): Promise<ResultOK<DynamoDB.DocumentClient.AttributeMap> | ResultFAIL<Error>> {
-        const params: DynamoDB.DocumentClient.UpdateItemInput = {
-            TableName: tableName,
-            Key: where,
-            AttributeUpdates: {},
-            ReturnValues: 'UPDATED_NEW',
-        };
-        const keys = Object.keys(data);
-        if (keys.length > 0) {
-            params.AttributeUpdates!['updatedAt'] = {
-                Action: 'PUT',
-                Value: new Date().toUTCString()
-            };
-            for (const key of keys) {
-                if (!!data[key]) {
-                    params.AttributeUpdates![key] = {
-                        Action: 'PUT',
-                        Value: data[key]
-                    };
-                } else {
-                    params.AttributeUpdates![key] = {
-                        Action: 'DELETE',
-                    };
-                }
-            }
-            const {Attributes} = await client.update(params).promise();
-            return ResultOk(Attributes!);
+  @tryCatchWrapperAsync
+  static async update(
+    client: DynamoDB.DocumentClient,
+    tableName: DynamoDB.DocumentClient.TableName,
+    where: DynamoDB.DocumentClient.Key,
+    data: { [key: string]: unknown }
+  ): Promise<ResultOK<DynamoDB.DocumentClient.AttributeMap> | ResultFAIL<Error>> {
+    const params: DynamoDB.DocumentClient.UpdateItemInput = {
+      TableName: tableName,
+      Key: where,
+      AttributeUpdates: {},
+      ReturnValues: 'UPDATED_NEW'
+    };
+    const keys = Object.keys(data);
+    if (keys.length > 0) {
+      params.AttributeUpdates!['updatedAt'] = {
+        Action: 'PUT',
+        Value: new Date().toUTCString()
+      };
+      for (const key of keys) {
+        if (data[key]) {
+          params.AttributeUpdates![key] = {
+            Action: 'PUT',
+            Value: data[key]
+          };
+        } else {
+          params.AttributeUpdates![key] = {
+            Action: 'DELETE'
+          };
         }
-        return ResultOk({});
+      }
+      const { Attributes } = await client.update(params).promise();
+      return ResultOk(Attributes!);
     }
+    return ResultOk({});
+  }
 }
-
